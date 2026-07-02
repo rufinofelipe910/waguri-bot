2226aee0dc4ee2b6ffde5a90f831a9a7fe25ce38
𝓡𝓮𝔂 𝓡𝓾𝓯𝓲𝓷𝓸 👑 <rufinofelipe910@gmail.com>
2026-07-02 21:32:50 +0100

    Actualizar Fun-memes.js

--- plugins/Fun-memes.js
+++ plugins/Fun-memes.js
@@ -1,36 +1,34 @@
-import hispamemes from 'hispamemes'
+// código creado por Rufino
 
-let handler = async (m, { conn, usedPrefix, command }) => {
-    // Tu lista de memes
-    const memesList = [
-        'https://cdn.hostrta.win/fl/ddmc.jpg',
-        'https://cdn.hostrta.win/fl/lmzb.jpg',
-        'https://cdn.hostrta.win/fl/esrf.jpg',
-        'https://cdn.hostrta.win/fl/00p5.jpg',
-        'https://cdn.hostrta.win/fl/bdog.jpg',
-        'https://cdn.hostrta.win/fl/ik2o.jpg',
-        'https://cdn.hostrta.win/fl/e7is.jpg',
-        'https://cdn.hostrta.win/fl/9q4i.jpg',
-        'cdn.hostrta.win/fl/o1dz.jpg',
-        'https://cdn.hostrta.win/fl/hj4s.jpg',
-        'https://cdn.hostrta.win/fl/cqck.jpg',
-        'https://cdn.hostrta.win/fl/97u6.jpg'
-    ]
-    
-    // Seleccionar un meme aleatorio
-    const randomIndex = Math.floor(Math.random() * memesList.length)
-    const memeUrl = memesList[randomIndex]
-    
-    // Enviar el meme
-    conn.sendFile(m.chat, memeUrl, 'meme.jpg', `😂 | ¡Meme aleatorio!`, m)
-    m.react('🌸')
+let handler = async (m, { conn }) => {
+  const memesList = [
+    'https://cdn.hostrta.win/fl/ddmc.jpg',
+    'https://cdn.hostrta.win/fl/lmzb.jpg',
+    'https://cdn.hostrta.win/fl/esrf.jpg',
+    'https://cdn.hostrta.win/fl/00p5.jpg',
+    'https://cdn.hostrta.win/fl/bdog.jpg',
+    'https://cdn.hostrta.win/fl/ik2o.jpg',
+    'https://cdn.hostrta.win/fl/e7is.jpg',
+    'https://cdn.hostrta.win/fl/9q4i.jpg',
+    'https://cdn.hostrta.win/fl/o1dz.jpg',
+    'https://cdn.hostrta.win/fl/hj4s.jpg',
+    'https://cdn.hostrta.win/fl/cqck.jpg',
+    'https://cdn.hostrta.win/fl/97u6.jpg'
+  ]
+
+  const randomIndex = Math.floor(Math.random() * memesList.length)
+  const memeUrl = memesList[randomIndex]
+
+  await conn.sendMessage(m.chat, {
+    image: { url: memeUrl },
+    caption: '😂 *¡Meme aleatorio!* 🌸'
+  }, { quoted: m })
 }
 
 handler.help = ['meme']
 handler.tags = ['fun']
 handler.command = ['meme', 'memes']
-handler.coin = 1
-handler.group = true;
+handler.group = true
 handler.register = true
 
 export default handler
